9b33c563f60999446e944cf7bd9d0034ef5431f2
min <kyu19681016@gmail.com>
2025-04-15 10:12:54 +0900

    Firse Release

---
+++ src/main/java/com/alpha/deuluwa/DeuluwaApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.deuluwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeuluwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeuluwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/deuluwa/DeuluwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.deuluwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeuluwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
